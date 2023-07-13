a16ac7ab8a40bcd57f8cea370ceb470e55806fa0
Felipe Lima <felipelima8556@gmail.com>
2023-07-13 11:02:30 -0300

    feat(documents): sockets rooms configureds

--- public/documento.js
+++ public/documento.js
@@ -1,9 +1,19 @@
-import { emitirTextoEditor } from "./socket-front-documento.js";
+import {
+  emitirTextoEditor,
+  selecionarDocumento,
+} from "./socket-front-documento.js";
+
+const parametros = new URLSearchParams(window.location.search);
+const documento = parametros.get("nome");
 
 const textoEditor = document.getElementById("editor-texto");
+const tituloDocumento = document.getElementById("titulo-documento");
+
+tituloDocumento.textContent = documento || "Documento sem titulo";
+selecionarDocumento(documento);
 
 textoEditor.addEventListener("keyup", () => {
-  emitirTextoEditor(textoEditor.value);
+  emitirTextoEditor({ texto: textoEditor.value, documento});
 });
 
 function atualizaTextoEditor(texto) {
--- src/socket-back.js
+++ src/socket-back.js
@@ -2,8 +2,15 @@ import io from "./server.js";
 
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou! ID:", socket.id);
-  socket.on("texto_editor", (texto) => {
-    socket.broadcast.emit("texto_editor_clientes", texto);
+
+  socket.on("texto_editor", ({texto, documento}) => {
+    //"socket.broadcast.emit("texto_editor_clientes", texto);
+    console.log(documento, texto)
+    socket.to(documento).emit("texto_editor_clientes", texto);
+  });
+
+  socket.on("selecionar_documento", (documento) => {
+    socket.join(documento);
   });
 
   socket.on("disconnect", (motivo) => {
