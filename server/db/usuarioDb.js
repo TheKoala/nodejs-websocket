03e4d24355ff6eb81dc3283997442afee915bee7
Felipe Lima <felipelima8556@gmail.com>
2023-08-17 09:39:51 -0300

    feat(register): password encrypt implement

--- server/db/usuarioDb.js
+++ server/db/usuarioDb.js
@@ -1,7 +1,11 @@
+import criarHashESalSenha from "../utils/senhaUtils.js";
 import { usuariosColecao } from "./dbconnect.js";
 
 function cadastrarUsuario({ nome, senha }) {
-  return usuariosColecao.insertOne({ nome, senha });
+
+  const { hashSenha, salSenha} = criarHashESalSenha(senha);
+
+  return usuariosColecao.insertOne({ nome, hashSenha, salSenha });
 }
 
 function encontrarUsuario(nome) {
